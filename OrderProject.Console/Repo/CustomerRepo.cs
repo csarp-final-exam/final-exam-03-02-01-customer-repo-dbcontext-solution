8885391deb47f3ce4db9d3ae237d763de76e4451
Csaba Gyuris <gyuris.csaba@vasvari.org>
2025-02-26 06:13:49 +0100

    CustomerRepo DbContext eléréssel

---
+++ OrderProject.Console/Repo/CustomerRepo.cs
@@ -0,0 +1,10 @@
+﻿using OrderProject.Console.Models;
+
+namespace OrderProject.Console.Repo
+{
+    public class CustomerRepo
+    {
+        private readonly OrderContext _context = new OrderContext();
+
+    }
+}
