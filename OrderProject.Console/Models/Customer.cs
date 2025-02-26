d3742cbdd88e05d10090f95585acf18356433fb3
Csaba Gyuris <gyuris.csaba@vasvari.org>
2025-02-26 05:49:14 +0100

    Scaffold

---
+++ OrderProject.Console/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OrderProject.Console.Models;
+
+public partial class Customer
+{
+    public string? Name { get; set; }
+
+    public string? Email { get; set; }
+
+    public int? Budget { get; set; }
+}
---
+++ OrderProject.Console/Models/OrderContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace OrderProject.Console.Models;
+
+public partial class OrderContext : DbContext
+{
+    public OrderContext()
+    {
+    }
+
+    public OrderContext(DbContextOptions<OrderContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlite("Data Source=Database\\order.db");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("customer");
+
+            entity.Property(e => e.Budget)
+                .HasColumnType("INT")
+                .HasColumnName("budget");
+            entity.Property(e => e.Email).HasColumnName("email");
+            entity.Property(e => e.Name).HasColumnName("name");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
